61f9fb5ab871f7822dd2b639ed1a72d1bc6c53e8
maullanailham <allanss132@gmail.com>
2024-03-04 14:17:28 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        kucing kucing = new kucing();
+        kucing.nama = "yayan";
+        kucing.age = 3;
+        kucing.address = " konoha ";
+        kucing.Jenis = " anggora";
+        kucing.kaki = 4;
+        kucing.makanbasah();
+    }
+}
---
+++ src/kucing.java
@@ -0,0 +1,16 @@
+public class kucing {
+    public String nama;
+    public int age;
+    public String address;
+    public String Jenis;
+    public int kaki;
+
+
+    public void makanbasah(){
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : "+ age);
+        System.out.println("Alamat :"+ address);
+        System.out.println("Jenis hewan :"+ Jenis);
+        System.out.println("Berkaki :"+kaki);
+    }
+}
